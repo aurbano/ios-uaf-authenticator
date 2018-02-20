9138481f56f311aa37e0702d2bde3b03e90a6a10
Iva Nikolaeva <iva.nikolaeva@student.manchester.ac.uk>
2018-02-20 09:46:47 +0000

    registration framework added

---
+++ RegistrationsDecoding.framework/Headers/RegistrationsDecoding.h
@@ -0,0 +1,19 @@
+//
+//  RegistrationsDecoding.h
+//  RegistrationsDecoding
+//
+//  Created by Iva on 26/09/2017.
+//  Copyright © 2017 Iva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RegistrationsDecoding.
+FOUNDATION_EXPORT double RegistrationsDecodingVersionNumber;
+
+//! Project version string for RegistrationsDecoding.
+FOUNDATION_EXPORT const unsigned char RegistrationsDecodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegistrationsDecoding/PublicHeader.h>
+
+
